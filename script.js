95c26866e34dec99eff4b50cd7725c050f0f2b08
Anamaria Bodescu <bodescu.anamariia@gmail.com>
2021-09-19 14:32:16 +0300

    feat: add functionality to the buttons

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const hidden = document.querySelector('.hidden');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal= document.querySelectorAll('.show-modal');
+
+const closeModal = function() {
+     modal.classList.add('hidden');
+     overlay.classList.add('hidden');
+};
+
+const openModal = function() {
+     console.log('Button Clicked');
+     modal.classList.remove('hidden');
+     overlay.classList.remove('hidden');
+};
+
+for(let i=0; i<btnsOpenModal.length; i++)
+     btnsOpenModal[i].addEventListener('click', openModal); 
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
